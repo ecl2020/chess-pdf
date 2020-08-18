e9b2e0e053cb51ad4d0535a6cde5a266b5aafc6c
ecl2020 <34615588+ecl2020@users.noreply.github.com>
2020-08-17 20:52:24 -0400

    testing out overleaf

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 f = open("./chess-game.tex", "w")
+with open("./chess-game.tex", "w") as file:
+    file.write("\\documentclass{article}\n")
+    file.write("\\begin{document}\n")
+    file.write("Hello Palo Alto!\n")
+    file.write("\\end{document}\n")
+
